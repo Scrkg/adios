181839f04f62044ec8f825e60e6622ce2b315aeb
dam2 <dam2@a223e13.dc.domenicoscarlatti.es>
2024-10-21 20:57:23 +0200

    Documentación inicial

--- src/adios/Adios.java
+++ src/adios/Adios.java
@@ -15,6 +15,9 @@ public class Adios {
      */
     public static void main(String[] args) {
         System.out.println("Adios");
+        System.out.println("Adios");
+        System.out.println("Adios");
+        System.out.println("Adios");
         // TODO code application logic here
     }
     
